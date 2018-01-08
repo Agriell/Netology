ab688d6206b3a4979a2b2e58d3ac59967c50581b
dmitrygalkin <galkin.dmitry.a@gmail.com>
2018-01-08 21:04:41 +0700

    home work 1.7

--- home_work_2.1/shoplist.py
+++ home_work_2.1/shoplist.py
@@ -3,24 +3,25 @@ def cook_book_generate():
     cook_book = {}
     with open('recipe.txt') as source:
         for line in source:
-            if len(line) == 1:
-                dish_name = source.readline().lower()
-                ingr_quantity = int(source.readline())
-                ingridients = []
-                for i in range(0, ingr_quantity):
-                    one_item = {}
-                    ing = source.readline().lower().strip().split(' | ')
-                    one_item['ingridient_name'] = ing[0]
-                    one_item['quantity'] = int(ing[1])
-                    one_item['measure'] = ing[2]
-                    ingridients.append(one_item)
-                # print(ingridients)
+            dish_name = line.lower()
+            ingr_quantity = int(source.readline())
+            ingridients = []
+            for i in range(ingr_quantity):
+                one_item = {}
+                ing = source.readline().lower().strip().split(' | ')
+                one_item['ingridient_name'] = ing[0]
+                one_item['quantity'] = int(ing[1])
+                one_item['measure'] = ing[2]
+                ingridients.append(one_item)
+            source.readline()
+            # print(ingridients)
             cook_book[dish_name.strip()] = ingridients
         # print(cook_book)
     return cook_book
 
 def get_shop_list_by_dishes(dishes, person_count):
   shop_list = {}
+  cook_book = cook_book_generate()
   for dish in dishes:
     for ingridient in cook_book[dish]:
       new_shop_list_item = dict(ingridient)
@@ -45,7 +46,6 @@ def create_shop_list():
   shop_list = get_shop_list_by_dishes(dishes, person_count)
   print_shop_list(shop_list)
 
-cook_book = cook_book_generate()
 
 create_shop_list()
 
