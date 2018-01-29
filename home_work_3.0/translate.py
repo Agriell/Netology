6bd18f6a581c07fdba701a59b5c7cd9888e01d52
dmitrygalkin <galkin.dmitry.a@gmail.com>
2018-01-30 00:12:06 +0700

    home_work_3.0

--- home_work_3.0/translate.py
+++ home_work_3.0/translate.py
@@ -5,7 +5,7 @@ API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa4334539
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 
-def translate_it(from_lang, to_lang, from_file_name):
+def translate_it(from_lang, to_lang, from_file_name, to_file_name):
 
     params = {
         'key': API_KEY,
@@ -18,6 +18,7 @@ def translate_it(from_lang, to_lang, from_file_name):
     print('')
     response = requests.get(URL, params=params)
     json_ = response.json()
+    save_file(to_file_name, ''.join(json_['text']))
     return ''.join(json_['text'])
 
 
@@ -30,10 +31,47 @@ def open_file(from_file_name):
         print('Чиатю файл: ', ffn)
     return (text)
 
+def save_file(to_file_name, text):
+    fullpath = os.path.abspath(os.path.dirname(__file__))
+    ffn = os.path.join(fullpath, to_file_name)
+    with open(ffn, 'w') as obj:
+        obj.write(text)
+        print('')
+        print('Записываю результат в файл: ', ffn)
+        print('')
+    return ()
 
-from_lang = 'de'
-to_lang = 'ru'
-from_file_name = 'de.txt'
-result = translate_it(from_lang, to_lang, from_file_name)
+data = [
+    {
+        'from_lang': 'de',
+        'to_lang': 'ru',
+        'from_file_name': 'de.txt',
+        'to_file_name': 'ru(de).txt'
+    },
+    {
+        'from_lang': 'es',
+        'to_lang': 'ru',
+        'from_file_name': 'es.txt',
+        'to_file_name': 'ru(es).txt'
+    },
+    {
+        'from_lang': 'fr',
+        'to_lang': 'ru',
+        'from_file_name': 'fr.txt',
+        'to_file_name': 'ru(fr).txt'
+    },
+]
 
-print(result)
+
+# from_lang = 'de'
+# to_lang = 'ru'
+# from_file_name = 'de.txt'
+# to_file_name = 'ru(de).txt'
+for item in data:
+    result = translate_it(
+        item['from_lang'],
+        item['to_lang'],
+        item['from_file_name'],
+        item['to_file_name']
+    )
+    print(result)
