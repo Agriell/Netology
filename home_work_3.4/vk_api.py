b659e07714816959329028359598502facd8ca28
dmitrygalkin <galkin.dmitry.a@gmail.com>
2018-02-10 22:07:01 +0700

    home_work_3.4

--- home_work_3.4/vk_api.py
+++ home_work_3.4/vk_api.py
@@ -8,17 +8,16 @@ authorisation_data = {
     'client_id': application_id,
     'display': 'page',
     'scope': 'friends',
-    'respose_type': 'token',
+    'response_type': 'token',
     'v': '5.71'
 }
 
 print('?'.join((authorization_url, urlencode(authorisation_data))))
 
+# https://oauth.vk.com/blank.html
+# #access_token=7796d3109e1e30d8336970479d6c9b6c30f14606bf84e1cdd4b19707ca65446202b1915ed56363d405ec8
+# &expires_in=86400&user_id=8514930
 
-token = '1234567890'
-
-first_men = 23456700
-second_men = 76543210
 
 def mutual_friends(first_men, second_men):
     params = {
@@ -26,7 +25,7 @@ def mutual_friends(first_men, second_men):
         'source_uid': first_men,
         'target_uid': second_men
     }
-    response = (requests.get('http://api.vk.com/method/friends.getMutual', params)).json()
+    response = ((requests.get('https://api.vk.com/method/friends.getMutual', params)).json())['response']
 
     return response
 
@@ -37,12 +36,19 @@ def user_url(id):
         'fields': 'domain'
     }
     response = (requests.get('http://api.vk.com/method/users.get', params)).json()
-    return response
+    # print(response)
+    i = response['response'][0]['domain']
+    url = ('https://vk.com/' + i)
+    return url
 
+
+token = '7796d3109e1e30d8336970479d6c9b6c30f14606bf84e1cdd4b19707ca65446202b1915ed56363d405ec8'
+
+first_men = 32327603
+second_men = 19120408
 
 print('Общие друзья:')
 for i in mutual_friends(first_men, second_men):
     # print(i)
-    print('id - {}, ссылка - {}'.format(i, user_url(i)['domain']))
-
+    print('id - {}, ссылка - {}'.format(i, user_url(i)))
 
