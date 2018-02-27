7fb3ba36c093fa5fd468a4054883be8e5d4815a6
dmitrygalkin <galkin.dmitry.a@gmail.com>
2018-02-28 01:18:02 +0700

    Домашка 3.2 - теперь не перезаписывает исходные картинки, а создает новые файлы в папке res

--- home_work_3.2_pic_converter/pic_convert.py
+++ home_work_3.2_pic_converter/pic_convert.py
@@ -2,20 +2,16 @@
 import glob
 import os.path
 import subprocess
-import sys
+
 
 # Пример использования для нашей задачи:
 # sips --resampleWidth 200 myphoto.jpg
 
-def create_file_list():  # return [list] with file names
-    fullpath = os.path.abspath(os.path.dirname(__file__))
-    # print(fullpath)
-    files = glob.glob(os.path.join(fullpath, 'Source', "*.jpg"))
-    # print(files)
-    return files
 
+fullpath = os.path.abspath(os.path.dirname(__file__))
+files = glob.glob(os.path.join(fullpath, 'Source', "*.jpg"))
+subprocess.Popen(['mkdir', 'res'])
 
-for file in create_file_list():
+for file in files:
     print(file)
-    subprocess.Popen(['sips', '--resampleWidth', '200', file])
-    # print(process.args)
+    subprocess.Popen(['sips', '--resampleWidth', '200', file, '--out', 'res/'])
